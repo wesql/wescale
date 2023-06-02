7b0b5f07c04ab29818b657a011f8f5631f8477cf
lyq10085 <2894975338@qq.com>
2023-06-02 16:20:04 +0800

    add vtconsensus e2e test run handler
    Signed-off-by: lyq10085 <2894975338@qq.com>

--- tools/wesql_vtconcensus_test.sh
+++ tools/wesql_vtconcensus_test.sh
@@ -3,4 +3,4 @@
 # run tests against a local cluster
 export VTDATAROOT="/tmp/"
 source build.env
-go run test.go -docker=false -follow  -db-flavor=wesql -shard vtconcensus
+go run test.go -docker=false -follow  -shard vtconcensus
