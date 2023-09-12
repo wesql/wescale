569360d4606b4840172356ec4f702427d1778e9d
earayu <earayu@163.com>
2023-09-12 16:40:33 +0800

    feat: remove ghost and ptosc
    Signed-off-by: earayu <earayu@163.com>

--- go/vt/vttablet/onlineddl/util.go
+++ go/vt/vttablet/onlineddl/util.go
@@ -1,4 +1,9 @@
 /*
+Copyright ApeCloud, Inc.
+Licensed under the Apache v2(found in the LICENSE file in the root directory).
+*/
+
+/*
 Copyright 2019 The Vitess Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,64 +25,12 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"time"
-
-	"vitess.io/vitess/go/vt/log"
 )
 
 const (
 	readableTimeFormat = "20060102150405"
 )
-
-// execCmd searches the PATH for a command and runs it, logging the output.
-// If input is not nil, pipe it to the command's stdin.
-func execCmd(name string, args, env []string, dir string, input io.Reader, output io.Writer) (cmd *exec.Cmd, err error) {
-	cmdPath, err := exec.LookPath(name)
-	if err != nil {
-		return cmd, err
-	}
-	log.Infof("execCmd: %v %v %v", name, cmdPath, args)
-
-	cmd = exec.Command(cmdPath, args...)
-	cmd.Env = env
-	cmd.Dir = dir
-	if input != nil {
-		cmd.Stdin = input
-	}
-	if output != nil {
-		cmd.Stdout = output
-		cmd.Stderr = output
-	}
-	err = cmd.Run()
-	if err != nil {
-		err = fmt.Errorf("failed running command: %v %s; error=%v", name, strings.Join(args, " "), err)
-		log.Errorf(err.Error())
-	}
-	log.Infof("execCmd success: %v", name)
-	return cmd, err
-}
-
-// createTempDir creates a temporary directory and returns its name
-func createTempDir(hint string) (dirName string, err error) {
-	if hint != "" {
-		return os.MkdirTemp("", fmt.Sprintf("online-ddl-%s-*", hint))
-	}
-	return os.MkdirTemp("", "online-ddl-*")
-}
-
-// createTempScript creates an executable file in given directory and with given text as content.
-func createTempScript(dirName, fileName, text string) (fullName string, err error) {
-	fullName = filepath.Join(dirName, fileName)
-	bytes := []byte(text)
-	err = os.WriteFile(fullName, bytes, 0755)
-	return fullName, err
-}
 
 // RandomHash returns a 64 hex character random string
 func RandomHash() string {
