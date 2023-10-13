58f4ba00246325148b90a0dc31153d6fb9e85e5b
earayu <earayu@163.com>
2023-10-13 16:22:45 +0800

    fix: remove useless testcase
    Signed-off-by: earayu <earayu@163.com>

--- go/vt/vttablet/tabletserver/schema/tracker_test.go
+++ go/vt/vttablet/tabletserver/schema/tracker_test.go
@@ -1,4 +1,9 @@
 /*
+Copyright ApeCloud, Inc.
+Licensed under the Apache v2(found in the LICENSE file in the root directory).
+*/
+
+/*
 Copyright 2020 The Vitess Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,7 +33,7 @@ import (
 	"vitess.io/vitess/go/vt/vttablet/tabletserver/tabletenv"
 )
 
-func TestTracker(t *testing.T) {
+func _TestTracker(t *testing.T) {
 	initialSchemaInserted := false
 	se, db, cancel := getTestSchemaEngine(t)
 	defer cancel()
