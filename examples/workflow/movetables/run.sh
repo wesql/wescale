8a82cf70d830551c306d96eee856e843d1896b5d
earayu <earayu@163.com>
2023-11-06 13:45:35 +0800

    fix: add demo

--- examples/workflow/movetables/run.sh
+++ examples/workflow/movetables/run.sh
@@ -4,3 +4,10 @@
 source "$(dirname "${BASH_SOURCE[0]:-$0}")/../../common/env.sh"
 
 vtctlclient --server localhost:15999 MoveTables -- --source movetables_source --tables 'customer,corder' Create movetables_target.source2target
+
+#GetRoutingRules
+vtctlclient --server localhost:15999 GetRoutingRules
+
+#TODO fixme: should be able to display the progress
+vtctlclient --server localhost:15999 MoveTables -- Progress movetables_target.source2target
+
