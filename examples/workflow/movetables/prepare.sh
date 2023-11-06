882a8ae7002675ae38671317f0f43bffcb73e783
earayu <earayu@163.com>
2023-11-06 11:04:55 +0800

    fix: add demo
    Signed-off-by: earayu <earayu@163.com>

--- examples/workflow/movetables/prepare.sh
+++ examples/workflow/movetables/prepare.sh
@@ -7,17 +7,23 @@ source "$(dirname "${BASH_SOURCE[0]:-$0}")/../../common/env.sh"
 mysql -h127.0.0.1 -P15306 -e 'create database if not exists movetables_source'
 mysql -h127.0.0.1 -P15306 -e 'create database if not exists movetables_target'
 
-mysql -h127.0.0.1 -P15306 -e 'create table movetables_source.t1(
-                              	c1 bigint primary key auto_increment,
-                              	c2 int not null default 1,
-                              	c3 int not null default 0
-                              );
-'
+mysql -h127.0.0.1 -P15306 -e 'create table if not exists movetables_source.product(
+                                sku varbinary(128),
+                                description varbinary(128),
+                                price bigint,
+                                primary key(sku)
+                              ) ENGINE=InnoDB;
+                              create table if not exists movetables_source.customer(
+                                customer_id bigint not null auto_increment,
+                                email varbinary(128),
+                                primary key(customer_id)
+                              ) ENGINE=InnoDB;
+                              create table if not exists movetables_source.corder(
+                                order_id bigint not null auto_increment,
+                                customer_id bigint,
+                                sku varbinary(128),
+                                price bigint,
+                                primary key(order_id)
+                              ) ENGINE=InnoDB;'
 sleep 1
-mysql -h127.0.0.1 -P15306 -e 'create table movetables_target.t1_shadow like movetables_source.t1'
-sleep 1
-
-mysql -h127.0.0.1 -P15306 -e 'insert into movetables_source.t1 values (null, 1, 1);'
-mysql -h127.0.0.1 -P15306 -e 'insert into movetables_source.t1 values (null, 1, 1);'
-mysql -h127.0.0.1 -P15306 -e 'insert into movetables_source.t1 values (null, 1, 1);'
 
