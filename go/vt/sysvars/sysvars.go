23de2601c2166198491d9904e18601de2797db38
newborn22 <953950914@qq.com>
2023-11-17 12:30:26 +0800

    fix: correct ReadWriteSplitForReadOnlyTxnUserInput system var name
    Signed-off-by: newborn22 <953950914@qq.com>

--- go/vt/sysvars/sysvars.go
+++ go/vt/sysvars/sysvars.go
@@ -101,7 +101,7 @@ var (
 	EnableDisplaySQLExecutionVTTabletType = SystemVariable{Name: "enable_display_sql_execution_vttablet_type", IsBoolean: true}
 
 	// user input to enable or disable read write splittikng for read only txn, but it only works when it is not in a txn
-	ReadWriteSplitForReadOnlyTxnUserInput = SystemVariable{Name: "enable_display_sql_execution_vttablet_type", IsBoolean: true}
+	ReadWriteSplitForReadOnlyTxnUserInput = SystemVariable{Name: "enable_read_write_splitting_for_read_only_txn", IsBoolean: true}
 
 	VitessAware = []SystemVariable{
 		Autocommit,
