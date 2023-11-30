2df8b7e5ac40fdc8ccd73f1f8f180f8b2bdf60ff
earayu <earayu@163.com>
2023-11-30 10:54:42 +0800

    feat: add demo for onlineddl
    Signed-off-by: earayu <earayu@163.com>

--- examples/onlineddl/utils.go
+++ examples/onlineddl/utils.go
@@ -1,15 +1,23 @@
+/*
+Copyright ApeCloud, Inc.
+Licensed under the Apache v2(found in the LICENSE file in the root directory).
+*/
+
+
 package main
 
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
-func createDatabaseIfNotExists(dsn string, dbName string) {
+func execute(dsn string, query string) (sql.Result, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	defer db.Close()
 
@@ -17,16 +25,16 @@ func createDatabaseIfNotExists(dsn string, dbName string) {
 	conn, err := db.Conn(ctx)
 	defer conn.Close()
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 
-	conn.ExecContext(ctx, "create database if not exists "+dbName)
+	return conn.ExecContext(ctx, query)
 }
 
-func executeOnlineDDL(dsn string, ddlStrategy, ddl string) string {
+func query(dsn string, query string) (*sql.Rows, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	defer db.Close()
 
@@ -34,13 +42,22 @@ func executeOnlineDDL(dsn string, ddlStrategy, ddl string) string {
 	conn, err := db.Conn(ctx)
 	defer conn.Close()
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 
-	conn.ExecContext(ctx, "set @@ddl_strategy='"+ddlStrategy+"'")
-	rows, err := conn.QueryContext(ctx, ddl)
+	return conn.QueryContext(ctx, query)
+}
+
+func createDatabaseIfNotExists(dsn string, dbName string) {
+	execute(dsn, "create database if not exists "+dbName)
+}
+
+func executeOnlineDDL(dsn string, ddlStrategy, ddl string) (string, error) {
+	execute(dsn, "set @@ddl_strategy='"+ddlStrategy+"'")
+
+	rows, err := query(dsn, ddl)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
 	defer rows.Close()
 
@@ -48,7 +65,71 @@ func executeOnlineDDL(dsn string, ddlStrategy, ddl string) string {
 	var uuid string
 	err = rows.Scan(&uuid)
 	if err != nil {
+		return "", err
+	}
+	return uuid, nil
+}
+
+func executeOnlineDDLThenWaitForCompletion(dsn string, ddlStrategy, ddl string) error {
+	uuid, err := executeOnlineDDL(dsn, ddlStrategy, ddl)
+	if err != nil {
+		return err
+	}
+	for {
+		rows, err := query(dsn, fmt.Sprintf("select migration_status from mysql.schema_migrations where migration_uuid = '%s'", uuid))
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+
+		if !rows.Next() {
+			return fmt.Errorf("migration %s not found", uuid)
+		}
+		var migrationStatus string
+		err = rows.Scan(&migrationStatus)
+		if err != nil {
+			return err
+		}
+		if migrationStatus == "complete" {
+			return nil
+		}
+		if migrationStatus == "queued" || migrationStatus == "ready" || migrationStatus == "running" {
+			continue
+		}
+		return fmt.Errorf("migration %s failed", uuid)
+	}
+}
+
+func continuousInsertData(dsn string, insertSQL string, parallelism int, duration time.Duration) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
 		panic(err.Error())
 	}
-	return uuid
+	defer db.Close()
+
+	ctx := context.Background()
+	conn, err := db.Conn(ctx)
+	defer conn.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	cancelCtx, cancel := context.WithCancel(context.Background())
+
+	insertFunc := func() {
+		for {
+			select {
+			case <-cancelCtx.Done():
+				return
+			default:
+				conn.ExecContext(cancelCtx, insertSQL)
+			}
+		}
+	}
+
+	for i := 1; i <= parallelism; i++ {
+		go insertFunc()
+	}
+	time.Sleep(duration)
+	cancel()
 }
