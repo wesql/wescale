67a460410b1bfaebd6c3fa1f4e87db4650c82ca7
geray <919179287@qq.com>
2023-12-14 18:20:53 +0800

    feat: support intrange,floatrange,generator,regex function in Branch
    Signed-off-by: <919179287@qq.com>

--- go/vt/vtgate/evalengine/func.go
+++ go/vt/vtgate/evalengine/func.go
@@ -819,6 +819,58 @@ func (builtinFake) call(env *ExpressionEnv, args []EvalResult, result *EvalResul
 	}
 	funcName := args[0].string()
 	switch strings.ToLower(funcName) {
+	case "date":
+		var formatPattern string
+		if len(args) == 2 {
+			formatPattern = args[1].string()
+		}
+		date := gofakeit.Date()
+		formattedDate := date.Format(formatPattern)
+		result.setString(formattedDate, collations.TypedCollation{
+			Collation:    collations.CollationUtf8ID,
+			Coercibility: collations.CoerceImplicit,
+			Repertoire:   collations.RepertoireASCII,
+		})
+	case "int8":
+		result.setInt64(int64(gofakeit.Int8()))
+	case "int16":
+		result.setInt64(int64(gofakeit.Int16()))
+	case "int32":
+		result.setInt64(int64(gofakeit.Int32()))
+	case "int64":
+		result.setInt64(gofakeit.Int64())
+	case "float32":
+		result.setFloat(float64(gofakeit.Float32()))
+	case "float64":
+		result.setFloat(gofakeit.Float64())
+	case "floatrange":
+		if len(args) < 3 {
+			throwArgError("float32range function need min and max parameters")
+		}
+		var min, max float64
+		if !sqltypes.IsNumber(args[1].typeof()) || !sqltypes.IsNumber(args[2].typeof()) {
+			throwArgError("float32range parameters type only support float")
+		}
+		min, ok := args[1].decimal().Float64()
+		if !ok {
+			throwArgError("float32range parameters min's type only support float")
+		}
+		max, ok = args[2].decimal().Float64()
+		if !ok {
+			throwArgError("float32range parameters min's type only support float")
+		}
+		result.setFloat(gofakeit.Float64Range(min, max))
+	case "intrange":
+		if len(args) < 3 {
+			throwArgError("float32range function need min and max parameters")
+		}
+		var min, max int
+		if !sqltypes.IsIntegral(args[1].typeof()) || !sqltypes.IsIntegral(args[2].typeof()) {
+			throwArgError("float32range parameters type only support int")
+		}
+		min = int(args[1].int64())
+		max = int(args[2].int64())
+		result.setInt64(int64(gofakeit.IntRange(min, max)))
 	case "name":
 		result.setString(gofakeit.Name(), collations.TypedCollation{
 			Collation:    collations.CollationUtf8ID,
@@ -837,6 +889,33 @@ func (builtinFake) call(env *ExpressionEnv, args []EvalResult, result *EvalResul
 			Coercibility: collations.CoerceImplicit,
 			Repertoire:   collations.RepertoireASCII,
 		})
+	case "regex":
+		if len(args) < 2 {
+			throwArgError("regex function need pattern function")
+		}
+		var pattern string
+		if len(args) == 2 {
+			pattern = args[1].string()
+		}
+		result.setString(gofakeit.Regex(pattern), collations.TypedCollation{
+			Collation:    collations.CollationUtf8ID,
+			Coercibility: collations.CoerceImplicit,
+			Repertoire:   collations.RepertoireASCII,
+		})
+	case "generator":
+		if len(args) < 2 {
+			throwArgError("generator function need pattern function")
+		}
+		var pattern string
+		if len(args) == 2 {
+			pattern = args[1].string()
+		}
+		result.setString(gofakeit.Generate(pattern), collations.TypedCollation{
+			Collation:    collations.CollationUtf8ID,
+			Coercibility: collations.CoerceImplicit,
+			Repertoire:   collations.RepertoireASCII,
+		})
+
 	}
 }
 func (builtinFake) typeof(env *ExpressionEnv, args []Expr) (sqltypes.Type, flag) {
@@ -850,8 +929,21 @@ func (builtinFake) typeof(env *ExpressionEnv, args []Expr) (sqltypes.Type, flag)
 	}
 	funcName := result.string()
 	switch strings.ToLower(funcName) {
-	case "name", "address", "uuid":
+	case "name", "address", "uuid", "regex", "generator", "date":
 		return sqltypes.VarChar, flagIntegerCap
+	case "int8":
+		return sqltypes.Int8, flagIntegerRange
+	case "int16":
+		return sqltypes.Int16, flagIntegerRange
+	case "int32", "intrange":
+		return sqltypes.Int32, flagIntegerRange
+	case "int64":
+		return sqltypes.Int64, flagIntegerRange
+	case "float32":
+		return sqltypes.Float32, flagNullable
+	case "float64", "floatrange":
+		return sqltypes.Float64, flagNullable
+
 	}
 	throwEvalError(fmt.Errorf("function %v is not support to get type", funcName))
 	return sqltypes.VarChar, flagIntegerCap
