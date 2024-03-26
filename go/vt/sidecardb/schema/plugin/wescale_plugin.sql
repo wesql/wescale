8f896acd66a4f76c2b622bba3396ae86c773ea3b
earayu <earayu@163.com>
2024-03-26 09:30:47 +0800

    fix: refactor system table mysql.wescale_plugin

--- go/vt/sidecardb/schema/plugin/wescale_plugin.sql
+++ go/vt/sidecardb/schema/plugin/wescale_plugin.sql
@@ -1,27 +1,26 @@
 CREATE TABLE IF NOT EXISTS mysql.wescale_plugin
 (
-    `id`                             bigint unsigned NOT NULL AUTO_INCREMENT,
-    `create_timestamp`               timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_timestamp`               timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `id`                     bigint unsigned NOT NULL AUTO_INCREMENT,
+    `create_timestamp`       timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_timestamp`       timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    `name`                           varchar(256) NOT NULL,
-    `description`                    text,
-    `priority`                       int NOT NULL DEFAULT 1000,
-    `status`                         varchar(64) NOT NULL comment 'ACTIVE, INACTIVE, DRY_RUN',
+    `name`                   varchar(256) NOT NULL,
+    `description`            text,
+    `priority`               int NOT NULL DEFAULT 1000,
+    `status`                 varchar(64) NOT NULL comment 'ACTIVE, INACTIVE, DRY_RUN',
 
-    `plans`                          text,
-    `tableNames`                     varchar(1024),
-    `sql_regex`                      text,
+    `plans`                  text,
+    `table_names`            text,
+    `query_regex`            text,
 
-    `request_ip_regex`               varchar(64),
-    `user_regex`                     varchar(64),
-    `leadingComment_regex`           text,
-    `trailingComment_regex`          text,
-    `bindVarConds`                   text,
+    `request_ip_regex`       varchar(64),
+    `user_regex`             varchar(64),
+    `leading_comment_regex`  text,
+    `trailing_comment_regex` text,
+    `bind_var_conds`         text,
 
-    `action`                         varchar(64) NOT NULL,
+    `action`                 varchar(64) NOT NULL comment 'CONTINUE, FAIL',
+    `action_args`            text,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`name`)
 ) ENGINE = InnoDB;
-
-# todo earayu: fix all table names to be uncapitalized
