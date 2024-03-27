faf15ad017a42777cde6f3d6fdd870f622ceb7ca
earayu <earayu@163.com>
2024-03-27 20:50:44 +0800

    fix: add testcase

--- go/vt/vttablet/customrule/databasecustomrule/databasecustomrule.go
+++ go/vt/vttablet/customrule/databasecustomrule/databasecustomrule.go
@@ -87,6 +87,66 @@ func (cr *databaseCustomRule) stop() {
 	cr.stopped.CompareAndSwap(false, true)
 }
 
+// queryResultToRule converts a query result to a rule.
+func queryResultToRule(row sqltypes.RowNamedValues) (*rules.Rule, error) {
+	ruleInfo := make(map[string]any)
+	ruleInfo["Name"] = row.AsString("name", "")
+	ruleInfo["Description"] = row.AsString("description", "")
+	ruleInfo["Priority"] = int(row.AsInt64("priority", 1000))
+	ruleInfo["Status"] = row.AsString("status", "")
+
+	// parse Plans
+	plansStringData := row.AsString("plans", "")
+	if plansStringData != "" {
+		plans, err := unmarshalArray(plansStringData)
+		if err != nil {
+			log.Errorf("Failed to unmarshal plans: %v", err)
+			return nil, err
+		}
+		ruleInfo["Plans"] = plans
+	}
+
+	// parse TableNames
+	tableNamesData := row.AsString("fully_qualified_table_names", "")
+	if tableNamesData != "" {
+		tables, err := unmarshalArray(tableNamesData)
+		if err != nil {
+			log.Errorf("Failed to unmarshal fully_qualified_table_names: %v", err)
+			return nil, err
+		}
+		ruleInfo["fullyQualifiedTableNames"] = tables
+	}
+
+	ruleInfo["Query"] = row.AsString("query_regex", "")
+	ruleInfo["QueryTemplate"] = row.AsString("query_template", "")
+	ruleInfo["RequestIP"] = row.AsString("request_ip_regex", "")
+	ruleInfo["User"] = row.AsString("user_regex", "")
+	ruleInfo["LeadingComment"] = row.AsString("leading_comment_regex", "")
+	ruleInfo["TrailingComment"] = row.AsString("trailing_comment_regex", "")
+
+	// parse BindVarConds
+	bindVarCondsData := row.AsString("bind_var_conds", "")
+	if bindVarCondsData != "" {
+		bindVarConds, err := unmarshalArray(bindVarCondsData)
+		if err != nil {
+			log.Errorf("Failed to unmarshal bind_var_conds: %v", err)
+			return nil, err
+		}
+		ruleInfo["BindVarConds"] = bindVarConds
+	}
+
+	ruleInfo["Action"] = row.AsString("action", "")
+	ruleInfo["ActionArgs"] = row.AsString("action_args", "")
+
+	rule, err := rules.BuildQueryRule(ruleInfo)
+	if err != nil {
+		log.Errorf("Failed to build rule: %v", err)
+		return nil, err
+	}
+
+	return rule, nil
+}
+
 func (cr *databaseCustomRule) applyRules(qr *sqltypes.Result) error {
 	qrs := rules.New()
 	for _, row := range qr.Named().Rows {
@@ -94,57 +154,8 @@ func (cr *databaseCustomRule) applyRules(qr *sqltypes.Result) error {
 			// We're not interested in the result any more.
 			return nil
 		}
-		ruleInfo := make(map[string]any)
-		ruleInfo["Name"] = row.AsString("name", "")
-		ruleInfo["Description"] = row.AsString("description", "")
-		ruleInfo["Priority"] = int(row.AsInt64("priority", 1000))
-		ruleInfo["Status"] = row.AsString("status", "")
-
-		// parse Plans
-		plansStringData := row.AsString("plans", "")
-		if plansStringData != "" {
-			plans, err := unmarshalArray(plansStringData)
-			if err != nil {
-				log.Errorf("Failed to unmarshal plans: %v", err)
-				continue
-			}
-			ruleInfo["Plans"] = plans
-		}
-
-		// parse TableNames
-		tableNamesData := row.AsString("fully_qualified_table_names", "")
-		if tableNamesData != "" {
-			tables, err := unmarshalArray(tableNamesData)
-			if err != nil {
-				log.Errorf("Failed to unmarshal fully_qualified_table_names: %v", err)
-				continue
-			}
-			ruleInfo["fullyQualifiedTableNames"] = tables
-		}
-
-		ruleInfo["Query"] = row.AsString("query_regex", "")
-		ruleInfo["QueryTemplate"] = row.AsString("query_template", "")
-		ruleInfo["RequestIP"] = row.AsString("request_ip_regex", "")
-		ruleInfo["User"] = row.AsString("user_regex", "")
-		ruleInfo["LeadingComment"] = row.AsString("leading_comment_regex", "")
-		ruleInfo["TrailingComment"] = row.AsString("trailing_comment_regex", "")
-
-		// parse BindVarConds
-		bindVarCondsData := row.AsString("bind_var_conds", "")
-		if bindVarCondsData != "" {
-			bindVarConds, err := unmarshalArray(bindVarCondsData)
-			if err != nil {
-				log.Errorf("Failed to unmarshal bind_var_conds: %v", err)
-				continue
-			}
-			ruleInfo["BindVarConds"] = bindVarConds
-		}
-
-		ruleInfo["Action"] = row.AsString("action", "")
-		ruleInfo["ActionArgs"] = row.AsString("action_args", "")
-		rule, err := rules.BuildQueryRule(ruleInfo)
+		rule, err := queryResultToRule(row)
 		if err != nil {
-			log.Errorf("Failed to build rule: %v", err)
 			continue
 		}
 		qrs.Add(rule)
