2e559471c86a08115299d0be015cee1c9ffdead5
earayu <earayu@163.com>
2024-03-28 15:00:51 +0800

    fix: add function: AfterExecution

--- go/vt/vttablet/tabletserver/action.go
+++ go/vt/vttablet/tabletserver/action.go
@@ -6,6 +6,7 @@ Licensed under the Apache v2(found in the LICENSE file in the root directory).
 package tabletserver
 
 import (
+	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/vttablet/tabletserver/rules"
 )
 
@@ -14,5 +15,13 @@ const DefaultPriority = 1000
 type ActionInterface interface {
 	BeforeExecution(qre *QueryExecutor) error
 
+	AfterExecution(reply *sqltypes.Result, err error) *ActionExecutionResponse
+
 	GetRule() *rules.Rule
+}
+
+type ActionExecutionResponse struct {
+	FireNext bool
+	Reply    *sqltypes.Result
+	Err      error
 }
