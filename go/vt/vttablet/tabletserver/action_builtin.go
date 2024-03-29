d20218c1c60b2d47217eaa591fe0f60dc14be916
earayu <earayu@163.com>
2024-03-29 16:28:02 +0800

    fix: implement ccl action

--- go/vt/vttablet/tabletserver/action_builtin.go
+++ go/vt/vttablet/tabletserver/action_builtin.go
@@ -101,7 +101,8 @@ type ConcurrencyControlAction struct {
 }
 
 func (p *ConcurrencyControlAction) BeforeExecution(qre *QueryExecutor) error {
-	doneFunc, waited, err := qre.tsv.qe.concurrencyController.Wait(qre.ctx, qre.plan.QueryTemplateID, []string{"t1"})
+	q := qre.tsv.qe.concurrencyController.GetOrCreateQueue(qre.plan.QueryTemplateID, p.maxConcurrency, p.maxConcurrency)
+	doneFunc, waited, err := q.Wait(qre.ctx, []string{"t1"})
 
 	if waited {
 		qre.tsv.stats.WaitTimings.Record("TxSerializer", time.Now())
