3eec33099dad58669f510a99271d19be3c18a47e
earayu <earayu@163.com>
2024-04-03 10:20:14 +0800

    fix: testcase

---
+++ go/vt/vttablet/tabletserver/action_builtin_test.go
@@ -0,0 +1,122 @@
+package tabletserver
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
+	vtrpcpb "vitess.io/vitess/go/vt/proto/vtrpc"
+	"vitess.io/vitess/go/vt/vterrors"
+	"vitess.io/vitess/go/vt/vttablet/tabletserver/rules"
+)
+
+func TestContinueAction(t *testing.T) {
+	action := &ContinueAction{}
+	qre := &QueryExecutor{}
+	assert.NoError(t, action.BeforeExecution(qre))
+	assert.Equal(t, &ActionExecutionResponse{
+		FireNext: true,
+	}, action.AfterExecution(qre, nil, nil))
+	assert.NoError(t, action.SetParams(""))
+	assert.Nil(t, action.GetRule())
+}
+
+func TestFailAction(t *testing.T) {
+	qr := rules.NewQueryRule("ruleDescription", "test_rule", rules.QRFail)
+
+	action := &FailAction{
+		Rule:   qr,
+		Action: rules.QRFail,
+	}
+	qre := &QueryExecutor{}
+	assert.EqualErrorf(t, action.BeforeExecution(qre), "disallowed due to rule: %s", qr.Description)
+	assert.Equal(t, &ActionExecutionResponse{
+		FireNext: true,
+	}, action.AfterExecution(qre, nil, nil))
+	assert.NoError(t, action.SetParams(""))
+	assert.NotNil(t, action.GetRule())
+}
+
+func TestFailRetryAction(t *testing.T) {
+	qr := rules.NewQueryRule("ruleDescription", "test_rule", rules.QRFailRetry)
+
+	action := &FailRetryAction{
+		Rule:   qr,
+		Action: rules.QRFailRetry,
+	}
+	qre := &QueryExecutor{}
+	err := action.BeforeExecution(qre)
+	assert.EqualErrorf(t, err, "disallowed due to rule: %s", qr.Description)
+	// check if the error is retried: Code_FAILED_PRECONDITION
+	errCode := vterrors.Code(err)
+	assert.Equal(t, vtrpcpb.Code_FAILED_PRECONDITION, errCode)
+
+	assert.Equal(t, &ActionExecutionResponse{
+		FireNext: true,
+	}, action.AfterExecution(qre, nil, nil))
+	assert.NoError(t, action.SetParams(""))
+	assert.NotNil(t, action.GetRule())
+}
+
+func TestConcurrencyControlAction(t *testing.T) {
+	qr := rules.NewQueryRule("ruleDescription", "test_rule", rules.QRConcurrencyControl)
+
+	action := &ConcurrencyControlAction{
+		Rule:           qr,
+		Action:         rules.QRConcurrencyControl,
+		MaxQueueSize:   2,
+		MaxConcurrency: 1,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	db := setUpQueryExecutorTest(t)
+	defer db.Close()
+	tsv := newTestTabletServer(ctx, noFlags, db)
+	qre := newTestQueryExecutor(ctx, tsv, "select * from t1 where a = :a and b = :b", 0)
+
+	// test with no concurrency control
+	assert.NoError(t, action.BeforeExecution(qre))
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	// test with concurrency control
+	go func() {
+		defer wg.Done()
+		timeOutErr := action.BeforeExecution(qre)
+		assert.EqualError(t, timeOutErr, "context deadline exceeded")
+	}()
+
+	// test with concurrency control and max queue size
+	go func() {
+		defer wg.Done()
+		time.Sleep(1 * time.Second)
+		maxQueueSizeErr := action.BeforeExecution(qre)
+		assert.EqualError(t, maxQueueSizeErr, "concurrency control protection: too many queued transactions (2 >= 2)")
+	}()
+
+	wg.Wait()
+
+	{
+		timeOutErr := action.BeforeExecution(qre)
+		assert.EqualError(t, timeOutErr, "context deadline exceeded")
+	}
+
+	assert.Equal(t, &ActionExecutionResponse{
+		FireNext: true,
+	}, action.AfterExecution(qre, nil, nil))
+
+	assert.NoError(t, action.BeforeExecution(qre))
+
+	assert.Equal(t, &ActionExecutionResponse{
+		FireNext: true,
+	}, action.AfterExecution(qre, nil, nil))
+}
+
+func TestConcurrencyControlActionSetParams(t *testing.T) {
+	action := &ConcurrencyControlAction{}
+	params := `{"max_queue_size": 2, "max_concurrency": 1}`
+	assert.NoError(t, action.SetParams(params))
+	assert.Equal(t, 2, action.MaxQueueSize)
+	assert.Equal(t, 1, action.MaxConcurrency)
+}
--- go/vt/vttablet/tabletserver/query_executor_test.go
+++ go/vt/vttablet/tabletserver/query_executor_test.go
@@ -40,11 +40,14 @@ import (
 	"vitess.io/vitess/go/mysql/fakesqldb"
 	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/callerid"
+	"vitess.io/vitess/go/vt/callinfo"
+	"vitess.io/vitess/go/vt/callinfo/fakecallinfo"
 	"vitess.io/vitess/go/vt/tableacl"
 	"vitess.io/vitess/go/vt/tableacl/simpleacl"
 	"vitess.io/vitess/go/vt/topo/memorytopo"
 	"vitess.io/vitess/go/vt/vterrors"
 	"vitess.io/vitess/go/vt/vttablet/tabletserver/planbuilder"
+	"vitess.io/vitess/go/vt/vttablet/tabletserver/rules"
 	"vitess.io/vitess/go/vt/vttablet/tabletserver/tabletenv"
 
 	querypb "vitess.io/vitess/go/vt/proto/query"
@@ -1095,6 +1098,59 @@ func TestQueryExecutorTableAclDryRun(t *testing.T) {
 	afterCount := tsv.stats.TableaclPseudoDenied.Counts()[tableACLStatsKey]
 	if afterCount-beforeCount != 1 {
 		t.Fatalf("table acl pseudo denied count should increase by one. got: %d, want: %d", afterCount, beforeCount+1)
+	}
+}
+
+func _TestQueryExecutorDenyListQRRetry(t *testing.T) {
+	db := setUpQueryExecutorTest(t)
+	defer db.Close()
+	query := "select * from test_table where name = 1 limit 1000"
+	expandedQuery := "select pk from test_table use index (`index`) where name = 1 limit 1000"
+	expected := &sqltypes.Result{
+		Fields: getTestTableFields(),
+	}
+	db.AddQuery(query, expected)
+	db.AddQuery(expandedQuery, expected)
+
+	db.AddQuery("select * from test_table where 1 != 1", &sqltypes.Result{
+		Fields: getTestTableFields(),
+	})
+
+	bannedAddr := "127.0.0.1"
+	bannedUser := "x"
+
+	alterRule := rules.NewQueryRule("disable update", "disable update", rules.QRFailRetry)
+	alterRule.SetIPCond(bannedAddr)
+	alterRule.SetUserCond(bannedUser)
+	alterRule.SetQueryCond("select.*")
+	alterRule.AddPlanCond(planbuilder.PlanSelect)
+	alterRule.AddTableCond("test_table")
+
+	rulesName := "denyListRulesQRRetry"
+	rules := rules.New()
+	rules.Add(alterRule)
+
+	callInfo := &fakecallinfo.FakeCallInfo{
+		Remote: bannedAddr,
+		User:   bannedUser,
+	}
+	ctx := callinfo.NewContext(context.Background(), callInfo)
+	tsv := newTestTabletServer(ctx, noFlags, db)
+	tsv.qe.queryRuleSources.UnRegisterSource(rulesName)
+	tsv.qe.queryRuleSources.RegisterSource(rulesName)
+	defer tsv.qe.queryRuleSources.UnRegisterSource(rulesName)
+
+	if err := tsv.qe.queryRuleSources.SetRules(rulesName, rules); err != nil {
+		t.Fatalf("failed to set rule, error: %v", err)
+	}
+
+	qre := newTestQueryExecutor(ctx, tsv, query, 0)
+	defer tsv.StopService()
+
+	assert.Equal(t, planbuilder.PlanSelect, qre.plan.PlanID)
+	_, err := qre.Execute()
+	if code := vterrors.Code(err); code != vtrpcpb.Code_FAILED_PRECONDITION {
+		t.Fatalf("tsv.qe.queryRuleSources.SetRules: %v, want %v", code, vtrpcpb.Code_FAILED_PRECONDITION)
 	}
 }
 
