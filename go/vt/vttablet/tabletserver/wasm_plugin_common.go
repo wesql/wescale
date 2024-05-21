be009d259aaf6adbb97c26093a07e04337875bf8
earayu <earayu@163.com>
2024-05-21 15:48:32 +0800

    fix: add host functions

--- go/vt/vttablet/tabletserver/wasm_plugin_common.go
+++ go/vt/vttablet/tabletserver/wasm_plugin_common.go
@@ -1,6 +1,17 @@
 package tabletserver
 
+import "unsafe"
+
 const AbiVersion = "v1alpha1"
 
 const WASMER = "wamser-go"
 const WAZERO = "wazero"
+
+func ptrToString(ptr uint32, size uint32) string {
+	return unsafe.String((*byte)(unsafe.Pointer(uintptr(ptr))), size)
+}
+
+func stringToPtr(s string) (uint32, uint32) {
+	ptr := unsafe.Pointer(unsafe.StringData(s))
+	return uint32(uintptr(ptr)), uint32(len(s))
+}
