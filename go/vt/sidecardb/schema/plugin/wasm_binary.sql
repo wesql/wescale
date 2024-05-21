d7e28a99c1de05026214b6994dec7e5fa7f70a34
newborn22 <953950914@qq.com>
2024-05-22 00:03:53 +0800

    fix: refactor tool functions; add some host functions; add wasm binary column

--- go/vt/sidecardb/schema/plugin/wasm_binary.sql
+++ go/vt/sidecardb/schema/plugin/wasm_binary.sql
@@ -5,7 +5,9 @@ CREATE TABLE IF NOT EXISTS mysql.wasm_binary
     `update_timestamp`                timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `name`                            varchar(256) NOT NULL,
     `runtime`                         text NOT NULL,
-    `data`                             MEDIUMTEXT NOT NULL,
+    `data`                             MEDIUMBLOB NOT NULL,
+    `compress_algorithm`    text NOT NULL,
+    `md5_hash_before_compress` varbinary(64) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`name`)
     ) ENGINE = InnoDB;
