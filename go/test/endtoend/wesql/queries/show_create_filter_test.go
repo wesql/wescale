213a3ef878825687ae430ab8872abce01fc72d6a
newborn22 <953950914@qq.com>
2024-06-23 14:47:50 +0800

    fix:fix testcase error about show create cmd content

--- go/test/endtoend/wesql/queries/show_create_filter_test.go
+++ go/test/endtoend/wesql/queries/show_create_filter_test.go
@@ -12,7 +12,7 @@ import (
 func TestShowCreateFilter(t *testing.T) {
 	execWithConnWithoutDB(t, func(conn *mysql.Conn) {
 
-		createSQL1 := `create filter ccl ( desc='test ccl', priority='999', status='ACTIVE' ) with_pattern ( plans='Select', fully_qualified_table_names='d1.t1', query_regex='', query_template='', request_ip_regex='', user_regex='', leading_comment_regex='', trailing_comment_regex='', bind_var_conds='' ) execute ( action='CONCURRENCY_CONTROL', action_args='max_queue_size=0;max_concurrency=0' )`
+		createSQL1 := "\ncreate filter ccl (\n\tdesc='test ccl',\n\tpriority='999',\n\tstatus='ACTIVE'\n)\nwith_pattern (\n\tplans='Select',\n\tfully_qualified_table_names='d1.t1',\n\tquery_regex='',\n\tquery_template='',\n\trequest_ip_regex='',\n\tuser_regex='',\n\tleading_comment_regex='',\n\ttrailing_comment_regex='',\n\tbind_var_conds=''\n)\nexecute (\n\taction='CONCURRENCY_CONTROL',\n\taction_args='max_queue_size=0;max_concurrency=0'\n)"
 		expectedSQL1 := createSQL1
 		_, err := conn.ExecuteFetch(createSQL1, 1000, true)
 		assert.Equal(t, nil, err)
@@ -25,7 +25,7 @@ func TestShowCreateFilter(t *testing.T) {
 		assert.Equal(t, expectedSQL1, got)
 
 		alterSQL1 := `alter filter ccl ( desc='test ccl', priority='999', status='ACTIVE') with_pattern ( plans='insert,update', fully_qualified_table_names='d1.t1,d2.*,*.t3', query_regex='.*', query_template='.*', request_ip_regex='.*', user_regex='.*', leading_comment_regex='.*', trailing_comment_regex='.*', bind_var_conds='' ) execute ( action='FAIL', action_args='max_queue_size=0;max_concurrency=0' )`
-		expectedSQL2 := `create filter ccl ( desc='test ccl', priority='999', status='ACTIVE' ) with_pattern ( plans='Insert,Update', fully_qualified_table_names='d1.t1,d2.*,*.t3', query_regex='.*', query_template='.*', request_ip_regex='.*', user_regex='.*', leading_comment_regex='.*', trailing_comment_regex='.*', bind_var_conds='' ) execute ( action='FAIL', action_args='' )`
+		expectedSQL2 := "\ncreate filter ccl (\n\tdesc='test ccl',\n\tpriority='999',\n\tstatus='ACTIVE'\n)\nwith_pattern (\n\tplans='Insert,Update',\n\tfully_qualified_table_names='d1.t1,d2.*,*.t3',\n\tquery_regex='.*',\n\tquery_template='.*',\n\trequest_ip_regex='.*',\n\tuser_regex='.*',\n\tleading_comment_regex='.*',\n\ttrailing_comment_regex='.*',\n\tbind_var_conds=''\n)\nexecute (\n\taction='FAIL',\n\taction_args=''\n)"
 		_, err = conn.ExecuteFetch(alterSQL1, 1000, true)
 		// action FAIL does not support action args
 		assert.NotNil(t, err)
@@ -42,7 +42,7 @@ func TestShowCreateFilter(t *testing.T) {
 		assert.Equal(t, expectedSQL2, got)
 
 		alterSQL2 := `alter filter ccl with_pattern ( plans='', fully_qualified_table_names='', query_regex='select \* .*', query_template='', request_ip_regex='127\.0\.0\..*', user_regex='.*', leading_comment_regex='haha.*', trailing_comment_regex='lala.*', bind_var_conds='' )`
-		expectedSQL3 := `create filter ccl ( desc='test ccl', priority='999', status='ACTIVE' ) with_pattern ( plans='', fully_qualified_table_names='', query_regex='select \* .*', query_template='', request_ip_regex='127\.0\.0\..*', user_regex='.*', leading_comment_regex='haha.*', trailing_comment_regex='lala.*', bind_var_conds='' ) execute ( action='FAIL', action_args='' )`
+		expectedSQL3 := "\ncreate filter ccl (\n\tdesc='test ccl',\n\tpriority='999',\n\tstatus='ACTIVE'\n)\nwith_pattern (\n\tplans='',\n\tfully_qualified_table_names='',\n\tquery_regex='select \\* .*',\n\tquery_template='',\n\trequest_ip_regex='127\\.0\\.0\\..*',\n\tuser_regex='.*',\n\tleading_comment_regex='haha.*',\n\ttrailing_comment_regex='lala.*',\n\tbind_var_conds=''\n)\nexecute (\n\taction='FAIL',\n\taction_args=''\n)"
 		_, err = conn.ExecuteFetch(alterSQL2, 1000, true)
 		assert.Equal(t, nil, err)
 		qr, err = conn.ExecuteFetch(showCreateFilterCCL, 1000, true)
