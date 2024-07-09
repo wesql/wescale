a9b2e286d48b20b82b2957d9aa0acc68d934f9d4
newborn22 <953950914@qq.com>
2024-07-09 17:16:28 +0800

    fix: refactor code about * expand

--- go/vt/vtgate/engine/custom_function_utils.go
+++ go/vt/vtgate/engine/custom_function_utils.go
@@ -15,38 +15,6 @@ import (
 	"vitess.io/vitess/go/vt/sqlparser"
 )
 
-// todo newborn22，
-// 函数目前支持所有的表达式作为参数，会交由mysql进行计算，wescale只负责将mysql计算结果作为函数输入的参数
-func GetParaExprFromFuncExpr(funcExpr *sqlparser.FuncExpr) ([]sqlparser.SelectExpr, error) {
-	rst := make([]sqlparser.SelectExpr, 0)
-	for _, expr := range funcExpr.Exprs {
-		switch expr.(type) {
-		case *sqlparser.AliasedExpr:
-			alias, _ := expr.(*sqlparser.AliasedExpr)
-			subFunc, ok := alias.Expr.(*sqlparser.FuncExpr)
-			if ok {
-				subExpr, err := GetParaExprFromFuncExpr(subFunc)
-				if err != nil {
-					return nil, err
-				}
-				rst = append(rst, subExpr...)
-			} else {
-				rst = append(rst, expr)
-			}
-		case *sqlparser.Nextval:
-			rst = append(rst, expr)
-		case *sqlparser.StarExpr:
-			return nil, errors.New("not support")
-		}
-	}
-	return rst, nil
-}
-
-func IsCustomFunctionName(fun string) bool {
-	_, exist := evalengine.CustomFunctions[fun]
-	return exist
-}
-
 func BuildVarCharFields(names ...string) []*querypb.Field {
 	fields := make([]*querypb.Field, len(names))
 	for i, v := range names {
@@ -97,6 +65,7 @@ func (c *CustomFunctionPrimitive) RemoveCustomFunction(stmt sqlparser.Statement)
 				c.TransferColName = append(c.TransferColName, true)
 
 			case *sqlparser.AliasedExpr:
+				//aliasExpr, _ := expr.(*sqlparser.AliasedExpr)
 				lookup := &evalengine.CustomFunctionParamLookup{}
 				_, err := evalengine.Translate(expr.(*sqlparser.AliasedExpr).Expr, lookup)
 				if err != nil {
@@ -125,104 +94,136 @@ func (c *CustomFunctionPrimitive) RemoveCustomFunction(stmt sqlparser.Statement)
 	}
 }
 
-func GetSelectExprColName(expr sqlparser.SelectExpr) string {
-	return sqlparser.String(expr)
-}
-
 // GetColNamesForStar the rst is empty if sqlExprs doesn't contain *;
-// todo newborn22 0705 多表*
 func (c *CustomFunctionPrimitive) GetColNamesForStar(ctx context.Context, vcursor VCursor) (map[string][]string, error) {
 	send, ok := c.Input.(*Send)
 	if !ok {
-		return nil, errors.New("CustomFunctionPrimitive's input is not send")
+		return nil, errors.New("CustomFunctionPrimitive's input is not Send primitive")
+	}
+
+	schemas := make([]string, 0)
+	names := make([]string, 0)
+	// map alias/name/schema.name to schema.names,
+	// when using * in a join case, a prefix of * can be mapped to more than one tables
+	map2QualifyTableName := make(map[string][]string)
+
+	for _, expr := range c.SentTables {
+		a, s, n, err := GetNamesOfTableExpr(expr)
+		if err != nil {
+			return nil, err
+		}
+		schemas = append(schemas, s...)
+		names = append(names, n...)
+		for i := range a {
+			qualifyName := fmt.Sprintf("%s.%s", s[i], n[i])
+			if a[i] != "" {
+				map2QualifyTableName[a[i]] = append(map2QualifyTableName[a[i]], qualifyName)
+			}
+			map2QualifyTableName[n[i]] = append(map2QualifyTableName[n[i]], qualifyName)
+			map2QualifyTableName[qualifyName] = append(map2QualifyTableName[qualifyName], qualifyName)
+		}
+	}
+
+	mapQualifyTableName2ColNames, err := GetColNamesForTable(ctx, send, vcursor, schemas, names)
+	if err != nil {
+		return nil, err
 	}
 
 	rst := make(map[string][]string)
 
 	for _, expr := range c.Sent {
 		if star, ok := expr.(*sqlparser.StarExpr); ok {
-
-			tableName := sqlparser.String(star.TableName)
-			if tableName == "" {
-				// * without prefix support sentTables that can be formatted to string
-				tableName = sqlparser.String(c.SentTables)
+			starPrefix := sqlparser.String(star.TableName)
+			if starPrefix == "" {
+				// then set all cols
+				for _, colNames := range mapQualifyTableName2ColNames {
+					rst[starPrefix] = append(rst[starPrefix], colNames...)
+				}
 			} else {
-				// * with a prefix, so the table expr should be alias type,
-				// * use in a join table will not go in this condition code
-				// todo newborn22, support more table expr types
-				tableAliasMap := make(map[string]string)
-				for _, t := range c.SentTables {
-					// todo newborn22, join
-					if alias, ok := t.(*sqlparser.AliasedTableExpr); ok {
-						tmpTableName, ok := alias.Expr.(sqlparser.TableName)
-						if !ok {
-							return nil, vterrors.Errorf(vtrpcpb.Code_INTERNAL, "BUG: the AST has changed. This should not be possible")
-						}
-
-						tableAlias := alias.As.String()
-						if tableAlias != "" {
-							tableAliasMap[tableAlias] = sqlparser.String(tmpTableName)
-						}
-						tableAliasMap[tmpTableName.Name.String()] = sqlparser.String(tmpTableName)
-						tableAliasMap[sqlparser.String(tmpTableName)] = sqlparser.String(tmpTableName)
-
-					} else {
-						return nil, errors.New("only support alias table expr when * with a prefix")
-					}
+				tables, exit := map2QualifyTableName[starPrefix]
+				if !exit {
+					return nil, fmt.Errorf("can not expand * for %v", star.TableName.Name.String())
 				}
-				var exist bool
-				_, exist = tableAliasMap[tableName]
-				if exist {
-					tableName = tableAliasMap[tableName]
+				for _, table := range tables {
+					rst[starPrefix] = append(rst[starPrefix], mapQualifyTableName2ColNames[table]...)
 				}
 			}
-
-			fieldsQuery := fmt.Sprintf("select * from %s limit 1", tableName)
-			rss, err := send.Resolve(ctx, vcursor)
-			if err != nil {
-				return nil, err
-			}
-			qr, err := execShard(ctx, send, vcursor, fieldsQuery, nil, rss[0], false /* rollbackOnError */, false /* canAutocommit */)
-			if err != nil {
-				return nil, err
-			}
-			names := make([]string, 0, len(qr.Fields))
-			for _, field := range qr.Fields {
-				names = append(names, field.Name)
-			}
-			rst[star.TableName.Name.String()] = names
 		}
 	}
 
 	return rst, nil
 }
 
-func InitCallExprForFuncExpr(expr *sqlparser.FuncExpr, offset *int, coll collations.TypedCollation) (*evalengine.CallExpr, error) {
-	// todo newborn22 0705 mysql func
-	f, exist := evalengine.CustomFunctions[expr.Name.String()]
-	if !exist {
-		return nil, errors.New("function not found in builtin funcitons")
-	}
-	args := make([]evalengine.Expr, 0)
-	// todo newborn22, 0705 translate
-	for _, para := range expr.Exprs {
-		if alias, ok := para.(*sqlparser.AliasedExpr); ok {
-			if subFunc, ok := alias.Expr.(*sqlparser.FuncExpr); ok {
-				subFuncCallExpr, err := InitCallExprForFuncExpr(subFunc, offset, coll)
-				if err != nil {
-					return nil, err
-				}
-				args = append(args, subFuncCallExpr)
-				continue
-			}
+// GetNamesOfTableExpr get alias, qualify, name of table expr,
+// only support alias and join type table expr,
+// for join, return each table's alias ("" for alias not set), qualify and name
+func GetNamesOfTableExpr(tableExpr sqlparser.TableExpr) ([]string, []string, []string, error) {
+	alias := make([]string, 0)
+	schemas := make([]string, 0)
+	names := make([]string, 0)
+	if aliasTableExpr, ok := tableExpr.(*sqlparser.AliasedTableExpr); ok {
+		tmpTableName, ok := aliasTableExpr.Expr.(sqlparser.TableName)
+		if !ok {
+			return nil, nil, nil, vterrors.Errorf(vtrpcpb.Code_INTERNAL, "BUG: the AST has changed. This should not be possible")
 		}
+		alias = append(alias, aliasTableExpr.As.String())
+		schemas = append(schemas, sqlparser.String(tmpTableName.Qualifier))
+		names = append(names, sqlparser.String(tmpTableName.Name))
+	} else if joinTableExpr, ok := tableExpr.(*sqlparser.JoinTableExpr); ok {
+		a1, s1, n1, err := GetNamesOfTableExpr(joinTableExpr.LeftExpr)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		a2, s2, n2, err := GetNamesOfTableExpr(joinTableExpr.RightExpr)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		alias = append(alias, a1...)
+		alias = append(alias, a2...)
+		schemas = append(schemas, s1...)
+		schemas = append(schemas, s2...)
+		names = append(names, n1...)
+		names = append(names, n2...)
+	} else {
+		return nil, nil, nil, errors.New("only support alias and join table expr")
+	}
+	return alias, schemas, names, nil
+}
 
-		colExpr := evalengine.NewColumn(*offset, coll)
-		*offset++
-		args = append(args, colExpr)
+// GetColNamesForTable return two maps:
+// mapQualifyTableName2ColNames: the key format is "qualify.name"
+func GetColNamesForTable(ctx context.Context, send *Send, vcursor VCursor, tableQualify []string, tableNames []string) (map[string][]string, error) {
+	if len(tableQualify) != len(tableNames) {
+		return nil, errors.New("tableQualify and tableNames must have the same length")
+	}
+	first := true
+	whereCondition := ""
+	for i, tableName := range tableNames {
+		if !first {
+			whereCondition += " or "
+		}
+		whereCondition += fmt.Sprintf("(TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s')", tableQualify[i], tableName)
+		first = false
 	}
 
-	rst := &evalengine.CallExpr{F: f, Arguments: args}
+	fieldsQuery := fmt.Sprintf("SELECT TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE %s ORDER BY ORDINAL_POSITION", whereCondition)
+	rss, err := send.Resolve(ctx, vcursor)
+	if err != nil {
+		return nil, err
+	}
+	qr, err := execShard(ctx, send, vcursor, fieldsQuery, nil, rss[0], false /* rollbackOnError */, false /* canAutocommit */)
+	if err != nil {
+		return nil, err
+	}
+
+	rst := make(map[string][]string)
+	for _, r := range qr.Named().Rows {
+		schema, _ := r.ToString("TABLE_SCHEMA")
+		name, _ := r.ToString("TABLE_NAME")
+		qualifyName := fmt.Sprintf("%v.%v", schema, name)
+		col, _ := r.ToString("COLUMN_NAME")
+		rst[qualifyName] = append(rst[qualifyName], col)
+	}
 	return rst, nil
 }
 
