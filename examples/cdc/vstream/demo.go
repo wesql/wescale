81f2af3d3ce0a88e7ab56a07420b3b81cff45113
earayu <earayu@163.com>
2024-07-22 10:06:04 +0800

    feat: add demo
    Signed-off-by: earayu <earayu@163.com>

--- examples/cdc/vstream/demo.go
+++ examples/cdc/vstream/demo.go
@@ -7,6 +7,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/spf13/pflag"
 	"github.com/tmc/langchaingo/vectorstores"
 	"google.golang.org/grpc"
 	"io"
@@ -26,6 +27,19 @@ import (
 	"github.com/tmc/langchaingo/vectorstores/qdrant"
 )
 
+var tableSchema string
+var tableName string
+var filterStatement string
+var gtid string
+
+var weScaleUrl string
+
+var embeddingModel string
+var embeddingUrl string
+
+var vectorStoreUrl string
+var vectorStoreCollectionName string
+
 var store vectorstores.VectorStore
 
 // create table t1 (c1 int primary key auto_increment, c2 text);
@@ -34,35 +48,37 @@ var store vectorstores.VectorStore
 // insert into t1 (c2) values ('I want you to act as an interviewer.');
 func main() {
 
-	initVectorStore()
+	test()
 
-	vgtid := &binlogdatapb.VGtid{
-		ShardGtids: []*binlogdatapb.ShardGtid{{
-			Keyspace: "d1",
-			Shard:    "0",
-			//Gtid:     "current",
-			Gtid: "",
-		}}}
-	filter := &binlogdatapb.Filter{
-		Rules: []*binlogdatapb.Rule{{
-			Match:  "t1",
-			Filter: "select * from t1",
-		}},
+	pflag.Parse()
+	err := checkFlags()
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
-	// 连接到 vtgate 服务
-	conn, err := grpc.Dial("127.0.0.1:15991", grpc.WithInsecure())
+
+	// 1. Connect to the vtgate server.
+	client, closeFunc, err := openWeScaleClient()
 	if err != nil {
 		log.Fatalf("failed to connect to vtgate: %v", err)
 	}
-	defer conn.Close()
+	defer closeFunc()
+	// 2. Initialize the embedding service and the vector store.
+	initVectorStore()
 
-	// 创建 vtgate 客户端
-	client := vtgateservice.NewVitessClient(conn)
-	defer conn.Close()
-	flags := &vtgatepb.VStreamFlags{
-		//MinimizeSkew:      false,
-		//HeartbeatInterval: 60, //seconds
+	// 3. Create a VStream request.
+	vgtid := &binlogdatapb.VGtid{
+		ShardGtids: []*binlogdatapb.ShardGtid{{
+			Keyspace: tableSchema,
+			Shard:    "0",
+			Gtid:     gtid,
+		}}}
+	filter := &binlogdatapb.Filter{
+		Rules: []*binlogdatapb.Rule{{
+			Match:  tableName,
+			Filter: filterStatement,
+		}},
 	}
+	flags := &vtgatepb.VStreamFlags{}
 	req := &vtgatepb.VStreamRequest{
 		TabletType: topodatapb.TabletType_PRIMARY,
 		Vgtid:      vgtid,
@@ -71,6 +87,7 @@ func main() {
 	}
 	reader, err := client.VStream(context.Background(), req)
 
+	// 4. Read the stream and process the events.
 	var fields []*querypb.Field
 	for {
 		resp, err := reader.Recv()
@@ -100,6 +117,7 @@ func main() {
 					},
 				})
 				fmt.Printf("%v\n", res)
+				// 5. Embed the text and add it to the vector store.
 				upsertVector(store, res)
 			default:
 				fmt.Printf("event type: %v\n", event.Type)
@@ -108,14 +126,77 @@ func main() {
 	}
 }
 
+func init() {
+	pflag.StringVar(&tableSchema, "table-schema", "", "The table schema.")
+	pflag.StringVar(&tableName, "table-name", "", "The table name.")
+	pflag.StringVar(&filterStatement, "filter-statement", "", "The filter statement.")
+	pflag.StringVar(&gtid, "gtid", "", "The GTID.")
+	pflag.StringVar(&weScaleUrl, "we-scale-url", "", "The WeScale URL.")
+	pflag.StringVar(&embeddingModel, "embedding-model", "", "The embedding model.")
+	pflag.StringVar(&embeddingUrl, "embedding-url", "", "The embedding URL.")
+	pflag.StringVar(&vectorStoreUrl, "vector-store-url", "", "The vector store URL.")
+	pflag.StringVar(&vectorStoreCollectionName, "vector-store-collection-name", "", "The vector store collection name.")
+}
+
+func checkFlags() error {
+	if tableSchema == "" {
+		return fmt.Errorf("table-schema is required")
+	}
+	if tableName == "" {
+		return fmt.Errorf("table-name is required")
+	}
+	if filterStatement == "" {
+		return fmt.Errorf("filter-statement is required")
+	}
+	if weScaleUrl == "" {
+		return fmt.Errorf("we-scale-url is required")
+	}
+	if embeddingModel == "" {
+		return fmt.Errorf("embedding-model is required")
+	}
+	if embeddingUrl == "" {
+		return fmt.Errorf("embedding-url is required")
+	}
+	if vectorStoreUrl == "" {
+		return fmt.Errorf("vector-store-url is required")
+	}
+	if vectorStoreCollectionName == "" {
+		return fmt.Errorf("vector-store-collection-name is required")
+	}
+	return nil
+}
+
+func test() {
+	tableSchema = "d1"
+	tableName = "t1"
+	filterStatement = "select * from t1"
+	gtid = ""
+	weScaleUrl = "localhost:15991"
+	embeddingModel = "text-embedding-3-large"
+	embeddingUrl = "https://api.gptsapi.net/v1"
+	vectorStoreUrl = "http://127.0.0.1:6333/"
+	vectorStoreCollectionName = "t1_vector"
+}
+
+func openWeScaleClient() (vtgateservice.VitessClient, func(), error) {
+	conn, err := grpc.Dial(weScaleUrl, grpc.WithInsecure())
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to connect to vtgate: %v", err)
+	}
+	client := vtgateservice.NewVitessClient(conn)
+	closeFunc := func() {
+		conn.Close()
+	}
+	return client, closeFunc, nil
+}
+
 func initVectorStore() vectorstores.VectorStore {
 	if store != nil {
 		return store
 	}
 	opts := []openai.Option{
-		openai.WithModel("gpt-3.5-turbo-0125"),
-		openai.WithEmbeddingModel("text-embedding-3-large"),
-		openai.WithBaseURL("https://api.gptsapi.net/v1"),
+		openai.WithEmbeddingModel(embeddingModel),
+		openai.WithBaseURL(embeddingUrl),
 	}
 	llm, err := openai.New(opts...)
 	if err != nil {
@@ -128,13 +209,13 @@ func initVectorStore() vectorstores.VectorStore {
 	}
 
 	// Create a new Qdrant vector store.
-	url, err := url.Parse("http://127.0.0.1:6333/")
+	url, err := url.Parse(vectorStoreUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 	s, err := qdrant.New(
 		qdrant.WithURL(*url),
-		qdrant.WithCollectionName("t1_vector"),
+		qdrant.WithCollectionName(vectorStoreCollectionName),
 		qdrant.WithEmbedder(e),
 	)
 	if err != nil {
@@ -156,9 +237,7 @@ func upsertVector(store vectorstores.VectorStore, result *sqltypes.Result) {
 	_, err := store.AddDocuments(context.Background(), []schema.Document{
 		{
 			PageContent: text,
-			Metadata: map[string]any{
-				"area": 2342,
-			},
+			Metadata:    map[string]any{},
 		},
 	})
 	if err != nil {
