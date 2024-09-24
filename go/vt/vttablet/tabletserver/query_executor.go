9948c391ba8faa154d72ab6af90b85a95415ff11
Terry Gao <gaoxuanhit@qq.com>
2024-09-24 16:32:38 +0000

    fix: query_executor.go
    Signed-off-by: Terry Gao <gaoxuanhit@qq.com>

--- go/vt/vttablet/tabletserver/query_executor.go
+++ go/vt/vttablet/tabletserver/query_executor.go
@@ -30,7 +30,6 @@ import (
 	"sync"
 	"time"
 
-	"vitess.io/vitess/go/internal/global"
 	"vitess.io/vitess/go/vt/vttablet/jobcontroller"
 
 	"google.golang.org/protobuf/proto"
@@ -47,7 +46,6 @@ import (
 	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/tableacl"
 	"vitess.io/vitess/go/vt/vterrors"
-	"vitess.io/vitess/go/vt/vtgate"
 	"vitess.io/vitess/go/vt/vtgate/evalengine"
 	"vitess.io/vitess/go/vt/vttablet/tabletserver/connpool"
 	p "vitess.io/vitess/go/vt/vttablet/tabletserver/planbuilder"
@@ -915,28 +913,28 @@ func (qre *QueryExecutor) getReadAfterWriteGtid(sql string) (readAfterWriteGtid 
 
 	return qre.options.GetReadAfterWriteGtid()
 
-	if qre.options.TableReadAfterWriteGtidMap == nil {
-		return qre.options.GetReadAfterWriteGtid()
-	}
+	// if qre.options.TableReadAfterWriteGtidMap == nil {
+	// 	return qre.options.GetReadAfterWriteGtid()
+	// }
 
-	lastSeenGtid, err := vtgate.NewLastSeenGtid(global.DefaultFlavor)
+	// lastSeenGtid, err := vtgate.NewLastSeenGtid(global.DefaultFlavor)
 
-	if err != nil {
-		log.Exitf("Unable to create new LastSeenGtid: %v", err)
-	}
+	// if err != nil {
+	// 	log.Exitf("Unable to create new LastSeenGtid: %v", err)
+	// }
 
-	stmt, _ := sqlparser.Parse(sql)
-	allTables := sqlparser.CollectTables(stmt, "db")
-	for _, table := range allTables {
-		tableName := table.GetName()
-		gtid := qre.options.TableReadAfterWriteGtidMap[tableName]
-		lastSeenGtid.AddGtid(gtid)
-	}
+	// stmt, _ := sqlparser.Parse(sql)
+	// allTables := sqlparser.CollectTables(stmt, "db")
+	// for _, table := range allTables {
+	// 	tableName := table.GetName()
+	// 	gtid := qre.options.TableReadAfterWriteGtidMap[tableName]
+	// 	lastSeenGtid.AddGtid(gtid)
+	// }
 
-	var gtidSets []*mysql.GTIDSet
-	lastSeenGtid.CompressWithGtidSets(gtidSets)
+	// var gtidSets []*mysql.GTIDSet
+	// lastSeenGtid.CompressWithGtidSets(gtidSets)
 
-	return lastSeenGtid.String()
+	// return lastSeenGtid.String()
 }
 
 const WaitGtidTimeoutFlag = "1"
