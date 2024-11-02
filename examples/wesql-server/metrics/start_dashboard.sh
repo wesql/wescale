722ac7499f21b667a22dc8c724c931caefa5ae7d
earayu <earayu@163.com>
2024-11-02 15:55:16 +0800

    feat: support monitor

--- examples/wesql-server/metrics/start_dashboard.sh
+++ examples/wesql-server/metrics/start_dashboard.sh
@@ -10,11 +10,23 @@ docker run -d \
 
 
 # datasource: http://prometheus:9090
+#docker run -d \
+#  --name=grafana \
+#  --network=monitoring \
+#  -p 3000:3000 \
+#  -v ./prometheus-datasource.yaml:/etc/grafana/provisioning/datasources/prometheus-datasource.yaml \
+#  -e "GF_AUTH_ANONYMOUS_ENABLED=true" \
+#  -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" \
+#  -e "GF_AUTH_DISABLE_LOGIN_FORM=true" \
+#  grafana/grafana
+
 docker run -d \
   --name=grafana \
   --network=monitoring \
   -p 3000:3000 \
-  -v ./prometheus-datasource.yaml:/etc/grafana/provisioning/datasources/prometheus-datasource.yaml \
+  -v $PWD/prometheus-datasource.yaml:/etc/grafana/provisioning/datasources/prometheus-datasource.yaml \
+  -v $PWD/dashboards.yaml:/etc/grafana/provisioning/dashboards/dashboards.yaml \
+  -v $PWD/performance_overview.json:/etc/grafana/provisioning/dashboards/performance_overview.json \
   -e "GF_AUTH_ANONYMOUS_ENABLED=true" \
   -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" \
   -e "GF_AUTH_DISABLE_LOGIN_FORM=true" \
