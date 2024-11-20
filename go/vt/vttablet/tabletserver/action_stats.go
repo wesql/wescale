44dce7f5e69f15c85e2c4edd5deefb888216c83d
earayu <earayu@163.com>
2024-11-20 23:32:58 +0800

    feat: update wasm plugin

--- go/vt/vttablet/tabletserver/action_stats.go
+++ go/vt/vttablet/tabletserver/action_stats.go
@@ -10,8 +10,8 @@ type ActionStats struct {
 	FilterBeforeExecutionTiming *servenv.TimingsWrapper
 	FilterAfterExecutionTiming  *servenv.TimingsWrapper
 	FilterErrorCounts           *stats.CountersWithSingleLabel
-	QPSRates                    *stats.Rates
-	WasmMemorySize              *stats.CountersWithMultiLabels
+	FilterQPSRates              *stats.Rates
+	FilterWasmMemorySize        *stats.CountersWithMultiLabels
 }
 
 func NewActionStats(exporter *servenv.Exporter) *ActionStats {
@@ -19,8 +19,8 @@ func NewActionStats(exporter *servenv.Exporter) *ActionStats {
 		FilterBeforeExecutionTiming: exporter.NewTimings("FilterBeforeExecution", "Filter before execution timings", "Name"),
 		FilterAfterExecutionTiming:  exporter.NewTimings("FilterAfterExecution", "Filter before execution timings", "Name"),
 		FilterErrorCounts:           exporter.NewCountersWithSingleLabel("FilterErrorCounts", "filter error counts", "Name"),
-		WasmMemorySize:              exporter.NewCountersWithMultiLabels("WasmMemorySize", "Wasm memory size", []string{"Name", "BeforeOrAfter"}),
+		FilterWasmMemorySize:        exporter.NewCountersWithMultiLabels("FilterWasmMemorySize", "Wasm memory size", []string{"Name", "BeforeOrAfter"}),
 	}
-	stats.QPSRates = exporter.NewRates("FilterQps", stats.FilterBeforeExecutionTiming, 15*60/5, 5*time.Second)
+	stats.FilterQPSRates = exporter.NewRates("FilterQps", stats.FilterBeforeExecutionTiming, 15*60/5, 5*time.Second)
 	return stats
 }
