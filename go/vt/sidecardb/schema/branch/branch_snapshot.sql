cbfdebd0f1114e6ad5096c7f501af87c3af61a22
newborn22 <953950914@qq.com>
2024-12-01 20:39:03 +0800

    fix: fix sql synx error

--- go/vt/sidecardb/schema/branch/branch_snapshot.sql
+++ go/vt/sidecardb/schema/branch/branch_snapshot.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS mysql.branch_snapshot
     `database`         varchar(64)     NOT NULL,
     `table`           varchar(64)     NOT NULL,
     `create_table_sql` text           NOT NULL,
-    `update_timestamp`               timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `update_timestamp`               timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY(`name`, `database`, `table`)
     ) ENGINE = InnoDB;
